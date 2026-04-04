08b60e75475b7f47178c3bfd6a8edd4ea7dd84f6
danush-20 <danushsh18@gmail.com>
2026-04-04 21:40:03 +0530

    fix SimpleNotificationService import path

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { AuthProvider } from "./src/context/AuthContext";
 import AppNavigator from "./src/navigation/AppNavigator";
 import 'react-native-gesture-handler';
 import { ThemeProvider } from './src/context/ThemeContext';
-import SimpleNotificationService from './services/SimpleNotificationService';
-import { useJoinRequestListener } from './hooks/useJoinRequestListener';
+import SimpleNotificationService from './src/services/SimpleNotificationService';
+import { useJoinRequestListener } from './src/hooks/useJoinRequestListener';
 
 
 export default function App() {
